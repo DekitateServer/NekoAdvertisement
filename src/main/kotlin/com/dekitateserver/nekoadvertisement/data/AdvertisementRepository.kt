7ca7dbb5b6d85cfac2f21df0eef7555581abf694
ken-kentan <ken.kentan.k2@gmail.com>
2019-04-22 20:30:23 +0900

    update AdvertisementDao

--- src/main/kotlin/com/dekitateserver/nekoadvertisement/data/AdvertisementRepository.kt
+++ src/main/kotlin/com/dekitateserver/nekoadvertisement/data/AdvertisementRepository.kt
@@ -3,6 +3,8 @@ package com.dekitateserver.nekoadvertisement.data
 import com.dekitateserver.nekoadvertisement.NekoAdvertisementPlugin
 import com.dekitateserver.nekoadvertisement.data.dao.AdvertisementDao
 import com.dekitateserver.nekoadvertisement.data.model.Advertisement
+import org.bukkit.entity.Player
+import java.util.*
 
 class AdvertisementRepository(
     private val plugin: NekoAdvertisementPlugin
@@ -11,4 +13,10 @@ class AdvertisementRepository(
     private val dao = AdvertisementDao(plugin.database, plugin.logger)
 
     fun getAdvertisementList(): List<Advertisement> = dao.getAdvertisementList()
+
+    fun getAdvertisement(player: Player): Advertisement? = dao.getAdvertisement(player.uniqueId)
+
+    fun addAdvertisement(player: Player, content: String, expiredDate: Date): Boolean =
+        dao.insertAdvertisement(player.uniqueId, content, expiredDate)
+
 }
--- src/main/kotlin/com/dekitateserver/nekoadvertisement/data/dao/AdvertisementDao.kt
+++ src/main/kotlin/com/dekitateserver/nekoadvertisement/data/dao/AdvertisementDao.kt
@@ -28,9 +28,8 @@ class AdvertisementDao(
     private fun createTableIfNeeded() {
         try {
             database.connection.use {
-                it.prepareStatement(CREATE_TABLE_SQL).apply {
-                    executeUpdate()
-                    close()
+                it.prepareStatement(CREATE_TABLE_SQL).use { st ->
+                    st.executeUpdate()
                 }
             }
         } catch (e: SQLException) {
@@ -43,13 +42,12 @@ class AdvertisementDao(
 
         try {
             database.connection.use {
-                val st = it.prepareStatement("SELECT * FROM $TABLE_NAME WHERE is_delete IS FALSE AND expire_date > NOW() ORDER BY id")
-
-                val result = st.executeQuery()
-                while (result.next()) {
-                    adList.add(result.toAdvertisement())
+                it.prepareStatement("SELECT * FROM $TABLE_NAME WHERE is_delete IS FALSE AND expire_date > NOW() ORDER BY id").use { st ->
+                    val result = st.executeQuery()
+                    while (result.next()) {
+                        adList.add(result.toAdvertisement())
+                    }
                 }
-                st.close()
             }
         } catch (e: SQLException) {
             logger.error("Failed to get ad list.", e)
@@ -58,31 +56,33 @@ class AdvertisementDao(
         return adList
     }
 
-    fun insertAdvertisement(ad: Advertisement): Boolean {
+    fun getAdvertisement(owner: UUID): Advertisement? {
         try {
             database.connection.use {
-                val st = it.prepareStatement("INSERT INTO $TABLE_NAME (owner, content, expired_date, is_delete) VALUES (?, ?, ?, FALSE)").apply {
-                    setString(1, ad.owner.toString())
-                    setString(2, ad.content)
-                    setTimestamp(3, Timestamp(ad.expiredDate.time))
+                return it.prepareStatement("SELECT * FROM $TABLE_NAME WHERE owner = ? AND is_delete IS FALSE AND expire_date > NOW() LIMIT 1").use { st ->
+                    st.setString(1, owner.toString())
+
+                    val result = st.executeQuery()
+                    if (result.next()) result.toAdvertisement() else null
                 }
             }
         } catch (e: SQLException) {
-            logger.error("Failed to insert ad.", e)
+            logger.error("Failed to get ad.", e)
         }
 
-        return false
+        return null
     }
 
-    fun deleteAdvertisement(ad: Advertisement): Boolean {
+    fun insertAdvertisement(owner: UUID, content: String, expiredDate: Date): Boolean {
         try {
             database.connection.use {
-                val st = it.prepareStatement("UPDATE $TABLE_NAME SET is_delete = TRUE WHERE owner = ? AND is_delete IS FALSE AND expired_date > NOW()").apply {
-                    setString(1, ad.owner.toString())
-                }
+                val count = it.prepareStatement("INSERT INTO $TABLE_NAME (owner, content, expired_date, is_delete) VALUES (?, ?, ?, FALSE)").use { st ->
+                    st.setString(1, owner.toString())
+                    st.setString(2, content)
+                    st.setTimestamp(3, Timestamp(expiredDate.time))
 
-                val count = st.executeUpdate()
-                st.close()
+                    st.executeUpdate()
+                }
 
                 return count > 0
             }
@@ -93,10 +93,34 @@ class AdvertisementDao(
         return false
     }
 
+    fun updateAdvertisement(ad: Advertisement): Boolean {
+        try {
+            database.connection.use {
+                val count = it.prepareStatement("UPDATE $TABLE_NAME SET owner = ?, content = ?, expired_date = ?, is_delete = ? WHERE id = ?").use { st ->
+                    st.setString(1, ad.owner.toString())
+                    st.setString(2, ad.content)
+                    st.setTimestamp(3, Timestamp(ad.expiredDate.time))
+                    st.setBoolean(4, ad.isDelete)
+                    st.setInt(5, ad.id)
+
+                    st.executeUpdate()
+                }
+
+                return count > 0
+            }
+        } catch (e: SQLException) {
+            logger.error("Failed to update ad.", e)
+        }
+
+        return false
+    }
+
     private fun ResultSet.toAdvertisement() =
             Advertisement(
+                getInt(1),
                 UUID.fromString(getString(2)),
                 getString(3),
-                getTimestamp(4)
+                getTimestamp(4),
+                getBoolean(5)
             )
 }
