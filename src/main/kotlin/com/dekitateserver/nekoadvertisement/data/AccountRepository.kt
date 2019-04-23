81effe8324a8431c63982a1e14294349a5e9738c
ken-kentan <ken.kentan.k2@gmail.com>
2019-04-23 15:47:57 +0900

    add AccountDao

--- src/main/kotlin/com/dekitateserver/nekoadvertisement/data/AccountRepository.kt
+++ src/main/kotlin/com/dekitateserver/nekoadvertisement/data/AccountRepository.kt
@@ -1,12 +1,15 @@
 package com.dekitateserver.nekoadvertisement.data
 
 import com.dekitateserver.nekoadvertisement.NekoAdvertisementPlugin
+import com.dekitateserver.nekoadvertisement.data.dao.AccountDao
 import com.dekitateserver.nekoadvertisement.data.model.Account
 import com.dekitateserver.nekoadvertisement.data.model.AdvertiseFrequency
 import org.bukkit.entity.Player
 import java.util.*
 
 class AccountRepository(plugin: NekoAdvertisementPlugin) {
+
+    private val dao = AccountDao(plugin.database, plugin.logger)
 
     fun createAccountIfNeeded(uuid: UUID) {
 
---
+++ src/main/kotlin/com/dekitateserver/nekoadvertisement/data/dao/AccountDao.kt
@@ -0,0 +1,63 @@
+package com.dekitateserver.nekoadvertisement.data.dao
+
+import com.dekitateserver.nekoadvertisement.data.model.Account
+import com.dekitateserver.nekoadvertisement.data.model.AdvertiseFrequency
+import com.dekitateserver.nekolib.data.dao.BaseDao
+import com.dekitateserver.nekolib.data.dao.Database
+import com.dekitateserver.nekolib.util.error
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.util.*
+import java.util.logging.Logger
+
+class AccountDao(
+    private val database: Database,
+    private val logger: Logger
+) : BaseDao() {
+
+    private companion object {
+        const val TABLE_NAME = "advertisement_account"
+        const val CREATE_TABLE_SQL =
+            "CREATE TABLE IF NOT EXISTS advertisement_account (id VARCHAR(36) NOT NULL, frequency VARCHAR(6) NOT NULL, PRIMARY KEY (id))"
+    }
+
+    init {
+        createTableIfNeeded()
+    }
+
+    private fun createTableIfNeeded() {
+        try {
+            database.connection.use {
+                it.prepareStatement(CREATE_TABLE_SQL).use { st ->
+                    st.executeUpdate()
+                }
+            }
+        } catch (e: SQLException) {
+            logger.error("Failed to create table($TABLE_NAME).", e)
+        }
+    }
+
+    fun insert(id: UUID): Boolean {
+        try {
+            database.connection.use {
+                val count = it.prepareStatement("INSERT IGNORE INTO $TABLE_NAME (id, frequency) VALUES (?, ?)").use { st ->
+                    st.setString(1, id.toString())
+                    st.setString(2, AdvertiseFrequency.MIDDLE.name)
+
+                    st.executeUpdate()
+                }
+
+                return count > 0
+            }
+        } catch (e: SQLException) {
+            logger.error("Failed to insert.", e)
+        }
+
+        return false
+    }
+
+    private fun ResultSet.toAccount() = Account(
+        UUID.fromString(getString(1)),
+        AdvertiseFrequency.valueOf(getString(2))
+    )
+}
